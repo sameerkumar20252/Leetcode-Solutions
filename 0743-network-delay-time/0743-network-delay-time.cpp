3b66b2abbc8eb53acb149907acf2098261de2304
Sameer Kumar <sameerkumar20252@gmail.com>
2026-06-23 12:03:37 +0530

    Time: 106 ms (6.9%), Space: 40.7 MB (99.09%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        //Bellman-Ford
         vector<int> dis(n, INT_MAX);
         dis[k - 1] = 0;
 
-        vector<vector<pair<int,int>>> adj(n);
+        for(int i = 0; i < n; i++) {
+            for(vector<int>& time : times) {
+                int u = time[0]-1;
+                int v = time[1]-1;
+                int wt = time[2];
 
-        for(auto& e : times) {
-            adj[e[0]-1].push_back({e[1]-1,e[2]});
-        }
-
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        pq.push({0, k-1});
-
-        while(pq.size() > 0) {
-            int d = pq.top().first;
-            int u = pq.top().second;
-            pq.pop();
-
-            if(d > dis[u]) continue;
-
-            for(auto& e : adj[u]) {
-                if(dis[e.first] > dis[u] + e.second) {
-                    dis[e.first] = dis[u] + e.second;
-                    pq.push({dis[e.first], e.first});
+                if(dis[u] != INT_MAX && dis[v] > dis[u] + wt) {
+                    if(i == n-1) return -1;
+                    dis[v] = dis[u] + wt;
                 }
             }
         }
