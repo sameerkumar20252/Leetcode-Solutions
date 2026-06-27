e688a6191cccadcbae41cc942212b855e5aff280
Sameer Kumar <sameerkumar20252@gmail.com>
2026-06-27 14:31:50 +0530

    Time: 0 ms (100%), Space: 13 MB (86.49%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> par, rank;
+    int m = 0, n = 0;
+    int find(int x) {
+        if(par[x] == x) {
+            return x;
+        }
+        return par[x] = find(par[x]);
+    }
+
+    void unite(int a, int b) {
+        int parA = find(a);
+        int parB = find(b);
+
+        if(parA == parB) {
+            m = a, n = b;
+            return;
+        }
+
+        if(rank[parA] == rank[parB]) {
+            par[parB] = parA;
+            rank[parA]++;
+        }
+        else if(rank[parA] < rank[parB]){
+            par[parA] = parB;
+        }
+        else {
+            par[parB] = parA;
+        }
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int V = edges.size();
+
+        par.resize(V);
+        rank.resize(V);
+
+        for(int i = 0; i < V; i++) {
+            par[i] = i;
+            rank[i] = 0;
+        }
+
+        for(auto& e : edges) {
+            unite(e[0]-1,e[1]-1);
+        }
+
+        return {m+1,n+1};
+    }
+};
