e7cc04388eacea5bdc628e6429ac98e0e1a247c3
Sameer Kumar <sameerkumar20252@gmail.com>
2026-06-28 15:00:54 +0530

    Time: 0 ms (100%), Space: 19.2 MB (97.67%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,55 +1,23 @@
 class Solution {
 public:
-    vector<int> par, rank;
-    int find(int x) {
-        if(par[x] == x) {
-            return x;
-        }
+    void dfs(int i, vector<vector<int>>& graph, vector<bool>& vis) {
+        vis[i] = true;
 
-        return par[x] = find(par[x]);
-    }
-
-    void unite(int a, int b) {
-        int parA = find(a);
-        int parB = find(b);
-
-        if(parA == parB) return;
-
-        if(rank[parA] == rank[parB]) {
-            par[parB] = parA;
-            rank[parA]++;
-        }
-        else if(rank[parA] > rank[parB]) {
-            par[parB] = parA;
-        }
-        else {
-            par[parA] = parB;
-        }
-    }
-
-
-    int findCircleNum(vector<vector<int>>& graph) {
-        int n = graph[0].size();
-        int prov = 0;
-
-        par.resize(n);
-        rank.resize(n);
-
-        for(int i = 0; i < n; i++) {
-            par[i] = i;
-            rank[i] = 0;
-        }
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < i; j++) {
-                if(graph[i][j]) {
-                    unite(i, j);
-                }
+        for(int j = 0; j < graph.size(); j++) {
+            if(graph[i][j] && !vis[j]) {
+                dfs(j, graph, vis);
             }
         }
+    }
+    int findCircleNum(vector<vector<int>>& graph) {
+        int n = graph.size();
+        int prov = 0;
+        vector<bool> vis(n, false);
 
-        for(int i = 0; i < n; i++) {
-            if(par[i] == i) {
+
+        for(int j = 0; j < n; j++) {
+            if(!vis[j]) {
+                dfs(j, graph, vis);
                 prov++;
             }
         }
