6e0799f0ff43df50c9636b841dbc10030583a3f1
Sameer Kumar <sameerkumar20252@gmail.com>
2026-07-11 11:27:36 +0530

    Time: 35 ms (23.93%), Space: 24.7 MB (67.16%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,85 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n = grid.size();
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) {
+            return -1;
+        }
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        dist[0][0] = 1;
+        using T = tuple<int,int,int>;
+
+        priority_queue<T,vector<T>,greater<T>> pq;
+        pq.push({1,0,0});
+
+        while(pq.size() > 0) {
+            auto [d,i,j] = pq.top();
+            pq.pop();
+
+            if(d > dist[i][j]) {
+                continue;
+            }
+
+            //up
+            if(i > 0) {
+                if(dist[i-1][j] > dist[i][j] + 1 && grid[i-1][j] != 1) {
+                    dist[i-1][j] = dist[i][j] + 1;
+                    pq.push({dist[i-1][j],i-1,j});
+                }
+                if(j > 0) {
+                    if(dist[i-1][j-1] > dist[i][j] + 1 && grid[i-1][j-1] != 1) {
+                        dist[i-1][j-1] = dist[i][j] + 1;
+                        pq.push({dist[i-1][j-1],i-1,j-1}); 
+                    }
+                }
+                if(j < n - 1) {
+                    if(dist[i-1][j+1] > dist[i][j] + 1 && grid[i-1][j+1] != 1) {
+                        dist[i-1][j+1] = dist[i][j] + 1;
+                        pq.push({dist[i-1][j+1],i-1,j+1});
+                    }
+                }
+            }
+
+            //down
+            if(i < n-1) {
+                if(dist[i+1][j] > dist[i][j] + 1 && grid[i+1][j] != 1) {
+                    dist[i+1][j] = dist[i][j] + 1;
+                    pq.push({dist[i+1][j],i+1,j});
+                }
+                if(j > 0) {
+                    if(dist[i+1][j-1] > dist[i][j] + 1 && grid[i+1][j-1] != 1) {
+                        dist[i+1][j-1] = dist[i][j] + 1;
+                        pq.push({dist[i+1][j-1],i+1,j-1}); 
+                    }
+                }
+                if(j < n - 1) {
+                    if(dist[i+1][j+1] > dist[i][j] + 1 && grid[i+1][j+1] != 1) {
+                        dist[i+1][j+1] = dist[i][j] + 1;
+                        pq.push({dist[i+1][j+1],i+1,j+1});
+                    }
+                }
+            }
+
+            //left
+            if(j > 0) {
+                if(dist[i][j-1] > dist[i][j] + 1 && grid[i][j-1] != 1) {
+                    dist[i][j-1] = dist[i][j] + 1;
+                    pq.push({dist[i][j-1],i,j-1});
+                }
+            }
+
+            //right
+            if(j < n - 1) {
+                if(dist[i][j+1] > dist[i][j] + 1 && grid[i][j+1] != 1) {
+                    dist[i][j+1] = dist[i][j] + 1;
+                    pq.push({dist[i][j+1],i,j+1});
+                }
+            }
+        }
+
+        if(dist[n-1][n-1] == INT_MAX) {
+            return -1;
+        }
+        return dist[n-1][n-1];
+    }
+};
